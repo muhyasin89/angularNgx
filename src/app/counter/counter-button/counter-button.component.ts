156bdb99e82044a1c7680af1686b7e9f336212f1
muhyasin89 <muhyasin89@gmail.com>
2022-11-24 17:17:05 +0700

    [Refactoring]change refactoring

--- src/app/counter/counter-button/counter-button.component.ts
+++ src/app/counter/counter-button/counter-button.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { decrement, increment, reset } from '../state/counter.actions';
+import { CounterState } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-button',
@@ -8,7 +9,7 @@ import { decrement, increment, reset } from '../state/counter.actions';
   styleUrls: ['./counter-button.component.css']
 })
 export class CounterButtonComponent implements OnInit{
-  constructor(private store: Store<{counter: {counter:number}}>){
+  constructor(private store: Store<{counter: CounterState}>){
 
   }
 
--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,20 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { CounterState } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css']
 })
-export class CounterOutputComponent implements OnInit{
-  counter:number =0;
-  constructor(private store: Store<{counter: {counter:number}}>){
+export class CounterOutputComponent implements OnInit, OnDestroy{
+  counter: number;
+  counter$: Observable<{ counter: number; }>;
+  counterSubscription: Subscription;
+  constructor(private store: Store<{counter: CounterState}>){
 
   }
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe(data => {
+    this.counterSubscription = this.store.select('counter').subscribe(data => {
       this.counter = data.counter;
     });
+
+    this.counter$ = this.store.select('counter');
+  }
+
+  ngOnDestroy(): void {
+      if(this.counterSubscription){
+        this.counterSubscription.unsubscribe;
+      }
   }
 }
