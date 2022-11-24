0f8c34c99c4f09a8d6ae4805c286c7c93e238e03
muhyasin89 <muhyasin89@gmail.com>
2022-11-24 12:06:11 +0700

    [main]first init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { CounterComponent } from './counter/counter/counter.component';
+import { CounterButtonComponent } from './counter/counter-button/counter-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterOutputComponent,
+    CounterComponent,
+    CounterButtonComponent
   ],
   imports: [
     BrowserModule,
